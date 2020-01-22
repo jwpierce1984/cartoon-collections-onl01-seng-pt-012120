a27eef9a58c61a6d6ee4d140bad39168646d59c2
Justin Pierce <jwpierce1984@gmail.com>
2020-01-22 22:58:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,10 +1,18 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+dwarves = ["Doc", "Dopey", "Bashful", "Grumpy", "Sneezy", "Sleepy", "Happy"]
+
+def roll_call_dwarves(dwarves)# code an argument here
+dwarves.each_with_index do |dwarf, index|
+  puts "#{index + 1}. #{dwarf}"
+end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map { |call| call.capitalize + "!" }
 end
+summon_captain_planet(planeteer_calls)
+
 
 def long_planeteer_calls# code an argument here
   # Your code here
